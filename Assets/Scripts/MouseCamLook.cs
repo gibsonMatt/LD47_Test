9996bec44c2f13e82260f83873f123ad666cad18
Matt Gibson <gibsomat@indiana.edu>
2020-09-17 12:05:04 -0400

    initial

---
+++ Assets/Scripts/MouseCamLook.cs
@@ -0,0 +1,40 @@
+﻿/* 
+ * author : jiankaiwang
+ * description : The script provides you with basic operations 
+ *               of first personal camera look on mouse moving.
+ * platform : Unity
+ * date : 2017/12
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseCamLook : MonoBehaviour
+{
+    public float mouseSensitivity = 100f;
+
+    public Transform playerBody;
+
+    float xRotation = 0f;
+
+    private void Start()
+    {
+
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    public void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+
+}
